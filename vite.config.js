f83725cfa776e769a47cecbd18c35471ea51e048
eXIA008 <165605674+eXIA008@users.noreply.github.com>
2026-04-22 15:31:53 +0700

    feat: add projects page

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import fs from 'fs'
+
+const copyIndexTo404 = () => {
+  return {
+    name: 'copy-index-to-404',
+    closeBundle() {
+      fs.copyFileSync(path.resolve(__dirname, 'dist/index.html'), path.resolve(__dirname, 'dist/404.html'))
+    }
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), copyIndexTo404()],
   base: "/",
   resolve: {
     alias: {
